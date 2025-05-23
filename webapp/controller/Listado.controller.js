4e801f80c92c1c372eb2478866d504db63e9d5b4
Pablo Paris <pparis@blueboot.com>
2025-05-23 18:24:09 +0000

    Changes

--- webapp/controller/Listado.controller.js
+++ webapp/controller/Listado.controller.js
@@ -3,25 +3,34 @@ sap.ui.define([
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
     "sap/m/MessageToast",
-], (Controller, Filter, FilterOperator, MessageToast) => {
+    "sap/ui/core/Fragment",
+], (Controller, Filter, FilterOperator, MessageToast, Fragment) => {
     "use strict";
 
     return Controller.extend("salidademateriales.controller.Listado", {
         onInit() {
-            const oRouter = this.getOwnerComponent().getRouter();
-            oRouter.getRoute("RouteListado").attachPatternMatched(this.onRouteMatched, this);
+            this.openInitialFilters();
         },
-        onRouteMatched(oEvent) {
-            const args = oEvent.getParameter("arguments");
-            const queries = args['?query'];
-            if (!queries){
-                const localModel = this.getView().getModel("LocalModel");
-                const reservas = localModel.getProperty("/reservas");
-                localModel.setProperty("/filteredReservas", reservas)
-                return;
-            };
-            const { materialFrom, materialTo, centro } = queries;
+        async openInitialFilters(){
+            if (!this.initialFiltersDialog) {
+                this.initialFiltersDialog = await Fragment.load({
+                    name: "salidademateriales.view.fragments.listado.dialogs.InitialFilters",
+                    controller: this,
+                    id: "initialFilters"
+                });
+                this.getView().addDependent(this.initialFiltersDialog);
+            }
+            this.initialFiltersDialog.open();
+        },
+        onInitialBuscar(){
+            const materialFrom = Fragment.byId('initialFilters', 'materialFromInput')?.getValue();
+            const materialTo = Fragment.byId('initialFilters', 'materialToInput')?.getValue();
+            const centro = Fragment.byId('initialFilters', 'centroInput')?.getValue();
             this.filterLocalModel(parseInt(materialFrom), parseInt(materialTo), centro);
+            this.initialFiltersDialog.close();
+        },
+        onCloseInitialFilters(){
+            this.initialFiltersDialog.close();
         },
         isMaterialInRange(material, materialFrom, materialTo){
             const materialId = parseInt(material?.id);
